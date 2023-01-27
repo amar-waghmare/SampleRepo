641bb90e3153a29e26846c4fabe0f1640997bfbd
Amar Waghmare <amarwaghmare573@gmail.com>
2023-01-27 19:26:31 +0530

    Basic Task

---
+++ DemoProject/src/myPackage/MyClass.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class MyClass {
+
+	public static void main(String args[])
+	{
+		System.out.println("TestBase");
+		System.out.println("pom.xml");
+		System.out.println("Utilities");
+		System.out.println("Resources");
+		System.out.println("Suite");
+	}
+}
